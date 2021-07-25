9ef23e815ed58d2de504ce8b3119683732495673
weiqinghan <66492945+weiqinghan@users.noreply.github.com>
2021-07-25 02:09:41 -0400

    added more crystals

--- GatorJam21/Assets/Script/CheckObjects.cs
+++ GatorJam21/Assets/Script/CheckObjects.cs
@@ -7,6 +7,7 @@ public class CheckObjects : MonoBehaviour
     public void OnTriggerEnter2D(Collider2D other)
     {
         other.transform.parent = transform;
+        //other.enabled = false;
         transform.root.GetComponent<GameManagerSc>().GetState = State.Shorten;
     }
 }
